05a98c58b454aa2831f8729e7d1214bf69814242
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-06 06:00:11 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Sdfg
+admin.site.register(Sdfg)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Sdfg(models.Model):
+    'Generated Model'
+    sdfgh = models.BigIntegerField()
